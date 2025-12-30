070e5ba165e701da4b787f54cb1791eceff64399
Dimaslab <dimasixc22@gmail.com>
2025-12-30 16:08:26 +0700

    feat: Update Hero2 component text gradient for improved visual appeal

--- app/atoms/hero-v2.tsx
+++ app/atoms/hero-v2.tsx
@@ -4,7 +4,7 @@ export default function Hero2() {
             <div className="w-full h-[450px] sm:h-[550px] lg:h-[700px] xl:h-[800px]  bg-gradient-to-b from-[#000000] to-[#00000061]  relative z-[1] overflow-hidden">
                 <div className="relative z-[99] pt-[140px] sm:pt-[180px] md:pt-[200px] lg:pt-[180px] xl:pt-[200px]">
                     <h1 className="text-2xl sm:text-[2.5rem] lg:text-5xl xl:text-[3.5rem] mb-4 sm:mb-5 lg:mb-6 text-white font-extrabold px-4 sm:px-8 lg:px-16 xl:px-24">
-                        Discover the  <span className="bg-gradient-to-r from-red-600 to-red-800 bg-clip-text text-transparent">Soul of Indonesia</span>
+                        Discover the  <span className="bg-gradient-to-r from-red-600 to-red-400 bg-clip-text text-transparent">Soul of Indonesia</span>
                     </h1>
                     <p className="text-red sm:text-lg lg:text-2xl xl:text-[1.75rem] mb-6 sm:mb-8 lg:mb-8  px-4 sm:px-8 lg:px-16 xl:px-24">
                         Journey through 17,000 islands of rich culture, ancient
